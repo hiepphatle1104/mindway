0f01fe45483a05c8b83ae39a891bdb2cbf6c55eb
hiepphatle1104 <phatlee1104@gmail.com>
2025-08-17 22:37:15 +0700

    update generate data

--- api/gen.py
+++ api/gen.py
@@ -1,10 +1,25 @@
 import osmnx as ox
 
+# Places
 places = [
   "Phuong Gia Dinh, Ho Chi Minh City, Viet Nam",
   "Phuong Binh Loi Trung, Ho Chi Minh City, Viet Nam",
   "Phuong Binh Thanh, Ho Chi Minh City, Viet Nam",
+  "Phuong Thanh My Tay, Ho Chi Minh City, Viet Nam"
 ]
+path = "./data/map3.xml"
+
+# Download network
 G = ox.graph_from_place(places, network_type="drive")
 
-ox.io.save_graphml(G, filepath="./data/map3.graphml")
+ox.io.save_graphml(G, filepath=path)
+
+# Generate nodes and edeges
+G = ox.load_graphml(path)
+
+# Get Nodes and Egdes
+nodes, edges = ox.graph_to_gdfs(G)
+
+# Export
+nodes.to_file("./data/nodes.json", driver="GeoJSON")
+edges.to_file("./data/edges.json", driver="GeoJSON")
--- api/prediction.py
+++ api/prediction.py
@@ -1,1 +1,46 @@
 import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+# giả sử chỉ có 1 edge
+edges = [
+    {"u": 366369944, "v": 5463799657, "id": 32584226},
+]
+
+# thời gian start – end
+start = datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0)
+end = datetime(year=2025, month=1, day=2, hour=0, minute=0, second=0)  # 1 ngày
+interval = 15  # phút
+
+rows = []
+
+for edge in edges:
+    t = start
+    while t < end:
+        hour = t.hour
+
+        # Simulate Traffic Flow
+        if 7 <= hour < 9 or 17 <= hour < 19:  # giờ cao điểm
+            flow = np.random.poisson(20)
+            speed = np.random.normal(25, 5)  # kẹt xe
+        elif 22 <= hour or hour < 5:  # đêm
+            flow = np.random.poisson(5)
+            speed = np.random.normal(45, 5)
+        else:  # giờ bình thường
+            flow = np.random.poisson(10)
+            speed = np.random.normal(35, 5)
+
+        rows.append({
+            "u": edge["u"],
+            "v": edge["v"],
+            "id": edge["id"],
+            "timestamp": t,
+            "flow": max(flow, 0),
+            "speed": max(speed, 0),
+        })
+
+        t += timedelta(minutes=interval)
+
+df = pd.DataFrame(rows)
+df.to_csv("./dataset/traffic_data.csv", index=False)
+print(df.head())
