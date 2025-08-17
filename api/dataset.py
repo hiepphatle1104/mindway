8bada122f71420edea2aa0666326766fe6e21423
hiepphatle1104 <phatlee1104@gmail.com>
2025-08-17 22:11:19 +0700

    update file structure

--- api/dataset.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("thanhnguyen2612/traffic-flow-data-in-ho-chi-minh-city-viet-nam")
-
-print("Path to dataset files:", path)
